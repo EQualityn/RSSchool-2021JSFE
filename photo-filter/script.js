10a786f3b4f1734c40c7fde9bf2c01002ec4f661
Egor Kalitin <egor120202@gmail.com>
2021-04-24 15:23:21 +0300

    feat: add fullscreen

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,1 +1,11 @@
 alert("Hello");
+const FULLSCREEN = document.querySelector('.fullscreen');
+FULLSCREEN.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+})
